40400c3be316aa53a906e8b584a867e3862ce569
Rafe Colton <r.colton@modcloth.com>
2014-04-20 15:25:51 -0700

    Removing all of those bogus pending/plaecholder specs for bob
    implementing a null and regular version of bob - I've learned my lesson
    with the dockerclient - I should just assume I'll need an interface to
    make tests possible

--- bob_test.go
+++ bob_test.go
@@ -18,82 +18,16 @@ func TestBuilder(t *testing.T) {
 }
 
 var _ = Describe("Build", func() {
-	var ()
-
-	BeforeEach(func() {
-	})
-
-	Context("when", func() {
-		XIt("", func() {
-		})
-	})
-})
-
-var _ = Describe("CommandSequence", func() {
 	var (
 		subject *Builder
-		//invalidInstructions  *builderfile.Builderfile
-		//expectedInstructions = &parser.InstructionSet{
-		//DockerBuildOpts: []string{"--rm", "--no-cache"},
-		//DockerTagOpts:   []string{},
-		//Containers: map[string]builderfile.ContainerSection{
-		//"base": *&builderfile.ContainerSection{
-		//Dockerfile: "Dockerfile.base",
-		//Included:   []string{},
-		//Excluded: []string{
-		//"spec",
-		//"tmp",
-		//},
-		//Registry: "quay.io/modcloth",
-		//Project:  "style-gallery",
-		//Tags: []string{
-		//"base",
-		//},
-		//},
-		//"app": *&builderfile.ContainerSection{
-		//Dockerfile: "Dockerfile",
-		//Included:   []string{},
-		//Excluded: []string{
-		//"spec",
-		//"tmp",
-		//},
-		//Registry: "quay.io/modcloth",
-		//Project:  "style-gallery",
-		//Tags: []string{
-		//"git describe --always",
-		//"git rev-parse -q --abbrev-ref HEAD",
-		//"git rev-parse -q HEAD",
-		//},
-		//},
-		//},
-		//}
 	)
 
 	BeforeEach(func() {
-		subject = NewBuilder()
+		subject = nil
 	})
 
-	Context("when determining commands from a valid instruction set", func() {
-		XIt("produces the correct command sequence", func() {
-			//sequence, _ := subject.CommandSequence(validInstructions)
-			//Expect(sequence).To(Equal([]string{}))
-		})
-
-		XIt("does not produce an error", func() {
-			//_, err := subject.CommandSequence(validInstructions)
-			//Expect(err).To(BeNil())
-		})
-	})
-
-	Context("when determining commands from an invalid instruction set", func() {
-		XIt("produces an empty command sequence", func() {
-			//sequence, _ := subject.CommandSequence(invalidInstructions)
-			//Expect(sequence).To(Equal([]string{}))
-		})
-
-		XIt("produces an error", func() {
-			//_, err := subject.CommandSequence(invalidInstructions)
-			//Expect(err).ToNot(BeNil())
+	Context("when", func() {
+		It("", func() {
 		})
 	})
 })
