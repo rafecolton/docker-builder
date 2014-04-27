3efa8bd049afc0875ce138816fc65df23d97584e
Rafe Colton <r.colton@modcloth.com>
2014-04-27 10:12:35 -0700

    More reorganization of the main package

--- builder/main.go
+++ builder/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	builder "github.com/rafecolton/bob"
 	"github.com/rafecolton/bob/config"
-	"github.com/rafecolton/bob/log"
 	"github.com/rafecolton/bob/parser"
 	"github.com/rafecolton/bob/version"
 )
@@ -24,95 +22,6 @@ var runtime *config.Runtime
 var ver *version.Version
 var par *parser.Parser
 var runAsWorker = flag.Bool("work", false, "Run as a Goworker")
-var allTheThings = func(queue string, args ...interface{}) (fake error) {
-	if queue == "docker-build" {
-		first := args[0].(map[string]interface{})
-		pwd := first["pwd"].(string)
-		build := fmt.Sprintf("%s/%s", pwd, first["build"].(string))
-
-		os.Setenv("PWD", pwd)
-
-		logger := log.Initialize(false)
-
-		par, err := parser.NewParser(build, logger)
-		if err != nil {
-			logger.Println(
-				color.Sprintf("@{r!}Alas, could not generate parser@{|}\n----> %+v", err),
-			)
-			gocleanup.Exit(73)
-		}
-
-		commandSequence, err := par.Parse()
-		if err != nil {
-			runtime.Println(color.Sprintf("@{r!}Alas, could not parse@{|}\n----> %+v", err))
-			gocleanup.Exit(23)
-		}
-
-		bob := builder.NewBuilder(logger, true)
-		bob.Builderfile = build
-
-		if err = bob.Build(commandSequence); err != nil {
-			logger.Println(
-				color.Sprintf(
-					"@{r!}Alas, I am unable to complete my assigned build because of...@{|}\n----> %+v",
-					err,
-				),
-			)
-			gocleanup.Exit(29)
-		}
-
-		return
-	}
-
-	// if user requests version/branch/rev
-	if runtime.Version {
-		runtime.Println(ver.Version)
-	} else if runtime.VersionFull {
-		runtime.Println(ver.VersionFull)
-	} else if runtime.Branch {
-		runtime.Println(ver.Branch)
-	} else if runtime.Rev {
-		runtime.Println(ver.Rev)
-	} else if runtime.Lintfile != "" {
-		// lint
-		par, _ = parser.NewParser(runtime.Lintfile, runtime)
-		par.AssertLint()
-	} else {
-		if runtime.Builderfile == "" {
-			runtime.Builderfile = "bob.toml"
-		}
-		// otherwise, build
-		par, err := parser.NewParser(runtime.Builderfile, runtime)
-		if err != nil {
-			runtime.Println(
-				color.Sprintf("@{r!}Alas, could not generate parser@{|}\n----> %+v", err),
-			)
-			gocleanup.Exit(73)
-		}
-
-		commandSequence, err := par.Parse()
-		if err != nil {
-			runtime.Println(color.Sprintf("@{r!}Alas, could not parse@{|}\n----> %+v", err))
-			gocleanup.Exit(23)
-		}
-
-		bob := builder.NewBuilder(runtime, true)
-		bob.Builderfile = runtime.Builderfile
-
-		if err = bob.Build(commandSequence); err != nil {
-			runtime.Println(
-				color.Sprintf(
-					"@{r!}Alas, I am unable to complete my assigned build because of...@{|}\n----> %+v",
-					err,
-				),
-			)
-			gocleanup.Exit(29)
-		}
-	}
-
-	gocleanup.Exit(0)
-	return
-}
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "-work" {
