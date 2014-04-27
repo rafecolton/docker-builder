334e863fb7b8664fe8c0c044a0bb69fc5018041c
Rafe Colton <r.colton@modcloth.com>
2014-04-26 23:35:21 -0700

    Wat. Magic.

--- builder/main.go
+++ builder/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	builder "github.com/rafecolton/bob"
 	"github.com/rafecolton/bob/config"
+	"github.com/rafecolton/bob/log"
 	"github.com/rafecolton/bob/parser"
 	"github.com/rafecolton/bob/version"
 )
@@ -20,7 +21,6 @@ import (
 )
 
 func init() {
-	goworker.Register("DockerBuild", buildWorker)
 }
 
 var runtime *config.Runtime
@@ -29,8 +29,99 @@ var par *parser.Parser
 var runAsWorker = flag.Bool("work", false, "Run as a Goworker")
 
 func main() {
+	allTheThings := func(queue string, args ...interface{}) (errFake error) {
+		if queue == "docker-build" {
+			first := args[0].(map[string]interface{})
+			pwd := first["pwd"].(string)
+			build := fmt.Sprintf("%s/%s", pwd, first["build"].(string))
+
+			os.Setenv("PWD", pwd)
+
+			logger := log.Initialize(false)
+
+			par, err := parser.NewParser(build, logger)
+			if err != nil {
+				logger.Println(
+					color.Sprintf("@{r!}Alas, could not generate parser@{|}\n----> %+v", err),
+				)
+				gocleanup.Exit(73)
+			}
+
+			commandSequence, err := par.Parse()
+			if err != nil {
+				runtime.Println(color.Sprintf("@{r!}Alas, could not parse@{|}\n----> %+v", err))
+				gocleanup.Exit(23)
+			}
+
+			bob := builder.NewBuilder(logger, true)
+			bob.Builderfile = build
+
+			if err = bob.Build(commandSequence); err != nil {
+				logger.Println(
+					color.Sprintf(
+						"@{r!}Alas, I am unable to complete my assigned build because of...@{|}\n----> %+v",
+						err,
+					),
+				)
+				gocleanup.Exit(29)
+			}
+
+			return
+		}
+
+		// if user requests version/branch/rev
+		if runtime.Version {
+			runtime.Println(ver.Version)
+		} else if runtime.VersionFull {
+			runtime.Println(ver.VersionFull)
+		} else if runtime.Branch {
+			runtime.Println(ver.Branch)
+		} else if runtime.Rev {
+			runtime.Println(ver.Rev)
+		} else if runtime.Lintfile != "" {
+			// lint
+			par, _ = parser.NewParser(runtime.Lintfile, runtime)
+			par.AssertLint()
+		} else {
+			if runtime.Builderfile == "" {
+				runtime.Builderfile = "bob.toml"
+			}
+			// otherwise, build
+			par, err := parser.NewParser(runtime.Builderfile, runtime)
+			if err != nil {
+				runtime.Println(
+					color.Sprintf("@{r!}Alas, could not generate parser@{|}\n----> %+v", err),
+				)
+				gocleanup.Exit(73)
+			}
+
+			commandSequence, err := par.Parse()
+			if err != nil {
+				runtime.Println(color.Sprintf("@{r!}Alas, could not parse@{|}\n----> %+v", err))
+				gocleanup.Exit(23)
+			}
+
+			bob := builder.NewBuilder(runtime, true)
+			bob.Builderfile = runtime.Builderfile
+
+			if err = bob.Build(commandSequence); err != nil {
+				runtime.Println(
+					color.Sprintf(
+						"@{r!}Alas, I am unable to complete my assigned build because of...@{|}\n----> %+v",
+						err,
+					),
+				)
+				gocleanup.Exit(29)
+			}
+		}
+
+		gocleanup.Exit(0)
+		return
+	}
+
 	if len(os.Args) > 1 && os.Args[1] == "-work" {
 		flag.Parse()
+		goworker.Register("DockerBuild", allTheThings)
 		if err := goworker.Work(); err != nil {
 			fmt.Println(
 				color.Sprintf("@{r!}Alas, something went wrong :'(@{|}\n----> %+v", err),
@@ -42,56 +133,5 @@ func main() {
 	runtime = config.NewRuntime()
 	ver = version.NewVersion()
 
-	// if user requests version/branch/rev
-	if runtime.Version {
-		runtime.Println(ver.Version)
-	} else if runtime.VersionFull {
-		runtime.Println(ver.VersionFull)
-	} else if runtime.Branch {
-		runtime.Println(ver.Branch)
-	} else if runtime.Rev {
-		runtime.Println(ver.Rev)
-	} else if runtime.Lintfile != "" {
-		// lint
-		par, _ = parser.NewParser(runtime.Lintfile, runtime)
-		par.AssertLint()
-	} else {
-		if runtime.Builderfile == "" {
-			runtime.Builderfile = "bob.toml"
-		}
-		// otherwise, build
-		par, err := parser.NewParser(runtime.Builderfile, runtime)
-		if err != nil {
-			runtime.Println(
-				color.Sprintf("@{r!}Alas, could not generate parser@{|}\n----> %+v", err),
-			)
-			gocleanup.Exit(73)
-		}
-
-		commandSequence, err := par.Parse()
-		if err != nil {
-			runtime.Println(color.Sprintf("@{r!}Alas, could not parse@{|}\n----> %+v", err))
-			gocleanup.Exit(23)
-		}
-
-		bob := builder.NewBuilder(runtime, true)
-		bob.Builderfile = runtime.Builderfile
-
-		if err = bob.Build(commandSequence); err != nil {
-			runtime.Println(
-				color.Sprintf(
-					"@{r!}Alas, I am unable to complete my assigned build because of...@{|}\n----> %+v",
-					err,
-				),
-			)
-			gocleanup.Exit(29)
-		}
-	}
-
-	gocleanup.Exit(0)
-}
-
-func buildWorker(queue string, args ...interface{}) error {
-	fmt.Printf("queue: %q\nargs: %q\n", queue, args)
-	return nil
+	allTheThings("")
 }
