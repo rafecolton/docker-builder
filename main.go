af87268fecdb10c501e657944113461ad461978f
Rafe Colton <r.colton@modcloth.com>
2014-06-19 11:17:17 -0700

    Making logging configurable from the environment

--- main.go
+++ main.go
@@ -22,7 +22,47 @@ import (
 
 var ver = version.NewVersion()
 var par *parser.Parser
-var logger = logrus.New()
+var logger *logrus.Logger
+
+func init() {
+	//set logger defaults
+	logger = logrus.New()
+	logger.Formatter = &logrus.TextFormatter{ForceColors: true}
+
+	//set logger defaults from env
+	setLogger(os.Getenv("DOCKER_BUILDER_LOG_LEVEL"), os.Getenv("DOCKER_BUILDER_LOG_FORMAT"))
+}
+
+func setLogger(level, format string) {
+	switch level {
+	case "debug", "d":
+		logger.Level = logrus.Debug
+	case "info", "i":
+		logger.Level = logrus.Info
+	case "warn", "w":
+		logger.Level = logrus.Warn
+	case "error", "e":
+		logger.Level = logrus.Error
+	case "fatal", "f":
+		logger.Level = logrus.Fatal
+	case "panic", "p":
+		logger.Level = logrus.Panic
+	default:
+		logger.Level = logrus.Info
+	}
+
+	switch format {
+	case "text", "t":
+		logger.Formatter = new(logrus.TextFormatter)
+	case "json", "j":
+		logger.Formatter = new(logrus.JSONFormatter)
+	case "force-color", "fc":
+		logger.Formatter = &logrus.TextFormatter{ForceColors: true}
+	default:
+		logger.Formatter = &logrus.TextFormatter{ForceColors: true}
+	}
+
+}
 
 func main() {
 	app := cli.NewApp()
@@ -50,32 +90,7 @@ func main() {
 		}
 	}
 	app.Before = func(c *cli.Context) error {
-		switch c.String("log-level") {
-		case "debug", "d":
-			logger.Level = logrus.Debug
-		case "info", "i":
-			logger.Level = logrus.Info
-		case "warn", "w":
-			logger.Level = logrus.Warn
-		case "error", "e":
-			logger.Level = logrus.Error
-		case "fatal", "f":
-			logger.Level = logrus.Fatal
-		case "panic", "p":
-			logger.Level = logrus.Panic
-		default:
-			logger.Level = logrus.Info
-		}
-
-		switch c.String("log-format") {
-		case "text", "t":
-			logger.Formatter = &logrus.TextFormatter{ForceColors: true}
-		case "json", "j":
-			logger.Formatter = new(logrus.JSONFormatter)
-		default:
-			logger.Formatter = &logrus.TextFormatter{ForceColors: true}
-		}
-
+		setLogger(c.String("log-level"), c.String("log-format"))
 		return nil
 	}
 	app.Commands = []cli.Command{
