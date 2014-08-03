88694613ed27947c81f1b6db2879316992187835
Rafe Colton <r.colton@modcloth.com>
2014-08-03 06:30:46 +0100

    Adding comments for the linter

--- builder/sanitize.go
+++ builder/sanitize.go
@@ -6,13 +6,23 @@ import (
 )
 
 const (
-	DotDotSanitizeErrorMessage      = "bobfile path must not contain .."
+	// DotDotSanitizeErrorMessage is the error message used in errors that occur
+	// because a provided Bobfile path contains ".."
+	DotDotSanitizeErrorMessage = "bobfile path must not contain .."
+
+	// InvalidPathSanitizeErrorMessage is the error message used in errors that
+	// occur because a provided Bobfile path is invalid
 	InvalidPathSanitizeErrorMessage = "bobfile path is invalid"
-	SymlinkSanitizeErrorMessage     = "bobfile path must not contain symlinks"
+
+	// SymlinkSanitizeErrorMessage is the error message used in errors that
+	// occur because a provided Bobfile path contains symlinks
+	SymlinkSanitizeErrorMessage = "bobfile path must not contain symlinks"
 )
 
 var dotDotRegex = regexp.MustCompile("\\.\\.")
 
+// SanitizeBuilderfilePath checks for disallowed entries in the provided
+// Bobfile path and returns either a sanitized version of the path or an error
 func SanitizeBuilderfilePath(file string) (string, BuilderError) {
 	if dotDotRegex.MatchString(file) {
 		return "", &SanitizeError{message: DotDotSanitizeErrorMessage}
