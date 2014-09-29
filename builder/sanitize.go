44b5daa05a351b4e65c4509e043d367a1e8eb974
Rafe Colton <r.colton@modcloth.com>
2014-09-28 21:51:00 -0700

    Catching an error when path to Dockerfile is not sanitary

--- builder/sanitize.go
+++ builder/sanitize.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"os"
 	"path/filepath"
 	"regexp"
 )
@@ -17,6 +18,8 @@ const (
 	// SymlinkSanitizeErrorMessage is the error message used in errors that
 	// occur because a provided Bobfile path contains symlinks
 	SymlinkSanitizeErrorMessage = "file path must not contain symlinks"
+
+	DoesNotExistSanitizeErrorMessage = "file requested does not exist"
 )
 
 var dotDotRegex = regexp.MustCompile(`\.\.`)
@@ -45,8 +48,12 @@ func SanitizeTrustedFilePath(trustedFilePath *TrustedFilePath) (*TrustedFilePath
 
 	resolved, err := filepath.EvalSymlinks(abs)
 	if err != nil {
+		msg := InvalidPathSanitizeErrorMessage
+		if os.IsNotExist(err) {
+			msg = DoesNotExistSanitizeErrorMessage
+		}
 		return nil, &SanitizeError{
-			Message:  InvalidPathSanitizeErrorMessage,
+			Message:  msg,
 			error:    err,
 			Filename: file,
 		}
