23261c36c0846ebcefa8468115ede0f596ef8688
Rafe Colton <r.colton@modcloth.com>
2014-11-10 15:19:18 -0800

    Rewriting more tests without ginkgo

--- parser/uuid/uuid_test.go
+++ parser/uuid/uuid_test.go
@@ -1,38 +1,24 @@
 package uuid_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	. "github.com/rafecolton/docker-builder/parser/uuid"
 	"testing"
 )
 
-func TestBuilder(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "UUID Specs")
+func TestRandomGenerator(t *testing.T) {
+	var random = NewUUIDGenerator(true)
+	alpha, _ := random.NextUUID()
+	beta, _ := random.NextUUID()
+	if alpha == beta {
+		t.Errorf("expected random uuids to be different")
+	}
 }
 
-var _ = Describe("GenerateUUID", func() {
-	var (
-		seeded Generator
-		random Generator
-	)
-
-	Context("with a random uuid generator", func() {
-		It("generates a different uuid every time", func() {
-			random = NewUUIDGenerator(true)
-			alpha, _ := random.NextUUID()
-			beta, _ := random.NextUUID()
-			Expect(alpha).ToNot(Equal(beta))
-		})
-	})
-
-	Context("with a seeded uuid generator", func() {
-		It("generates the same uuid every time", func() {
-			seeded = NewUUIDGenerator(false)
-			alpha, _ := seeded.NextUUID()
-			beta, _ := seeded.NextUUID()
-			Expect(alpha).To(Equal(beta))
-		})
-	})
-})
+func TestSeededGenerator(t *testing.T) {
+	var seeded = NewUUIDGenerator(false)
+	alpha, _ := seeded.NextUUID()
+	beta, _ := seeded.NextUUID()
+	if alpha != beta {
+		t.Errorf("expected uuids from seeded generator to be identical")
+	}
+}
