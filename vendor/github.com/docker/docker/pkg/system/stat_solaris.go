6db4894333a0ea8c7d6f12058f9b3b15f624b971
Rafe Colton <rcolton@himalayaninstitute.org>
2017-07-25 12:41:08 -0400

    Update vendored docker libraries

--- vendor/github.com/docker/docker/pkg/system/stat_solaris.go
+++ vendor/github.com/docker/docker/pkg/system/stat_solaris.go
@@ -11,9 +11,3 @@ func fromStatT(s *syscall.Stat_t) (*StatT, error) {
 		rdev: uint64(s.Rdev),
 		mtim: s.Mtim}, nil
 }
-
-// FromStatT converts a syscall.Stat_t type to a system.Stat_t type
-// This is exposed on Linux as pkg/archive/changes uses it.
-func FromStatT(s *syscall.Stat_t) (*StatT, error) {
-	return fromStatT(s)
-}
--- vendor/github.com/docker/docker/pkg/system/syscall_unix.go
+++ vendor/github.com/docker/docker/pkg/system/syscall_unix.go
@@ -2,10 +2,16 @@
 
 package system
 
-import "syscall"
+import "golang.org/x/sys/unix"
 
 // Unmount is a platform-specific helper function to call
 // the unmount syscall.
 func Unmount(dest string) error {
-	return syscall.Unmount(dest, 0)
+	return unix.Unmount(dest, 0)
+}
+
+// CommandLineToArgv should not be used on Unix.
+// It simply returns commandLine in the only element in the returned array.
+func CommandLineToArgv(commandLine string) ([]string, error) {
+	return []string{commandLine}, nil
 }
